d62d5a713ac14750f6a7bbb8618120b7ebc8977f
linux-downey <1148256554@qq.com>
2018-05-09 10:35:12 +0800

    lib file

---
+++ Seeed_MPR121_driver.cpp
@@ -0,0 +1,241 @@
+/*
+ * Seeed_MPR121_driver.cpp
+ * Driver for DIGITAL I2C HUMIDITY AND TEMPERATURE SENSOR
+ *  
+ * Copyright (c) 2018 Seeed Technology Co., Ltd.
+ * Website    : www.seeed.cc
+ * Author     : downey
+ * Create Time: May 2018
+ * Change Log :
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "Seeed_MPR121_driver.h"
+
+Mpr121::Mpr121(u8 addr)
+{
+    _IIC_ADDR=addr;
+}
+
+void  Mpr121::begin()
+{
+    Wire.begin();
+    select_mode(STOP_MODE);
+    delay(100);
+    /*FFI-6,CDC-16UA,CDT-0.5US,SFI-4,ESI-4MS*/
+    set_globle_param(0x2310);
+    /*Touch debounce =2*SFI*ESI,Realese debounce=2*SFI*ESI */
+    set_debounce(0X22);
+    /*Set touch and realese threshold 0x08.*/
+    //set_threshold(0x0808);
+    //select_mode(START_PROXIMITY_DISABLE_MODE);
+    select_mode(START_PROXIMITY_ENABLE_MODE);
+}
+
+
+/**@brief Set sensor mode:STOP,detection,proximity.
+ * @param mode
+ * 
+ * */
+void Mpr121::select_mode(sensor_mode_t mode)
+{
+    switch(mode)
+    {
+        case STOP_MODE:
+        sensor_stop();
+        break;
+        case START_PROXIMITY_ENABLE_MODE:
+        sensor_start_proximity_enable();
+        break;
+        case START_PROXIMITY_DISABLE_MODE:
+        sensor_start_proximity_disable();
+        break;
+        default:break;
+    }
+}
+
+
+
+/*debounce_touch=(bit0~bit2)*EFI*SSI   debounce_release=(bit4~bit6)*ESI*SFI */
+/*Default ESI=16ms,SFI=4,This two parameters is configurable,see set_globle_param()*/
+/*ESI--Electrode Sample Interval,SFI--Second Filter Iterations*/
+
+/**@brief Set debounce of touch 
+ * @param debounce 
+ * 
+ * */
+void Mpr121::set_debounce(u8 debounce)
+{
+   IIC_write_byte(DEBOUNCE_REG_ADDR,debounce); 
+}
+
+
+/*  0x5d:
+    bit0~bit2:ESI
+    bit3~bit4:SFI
+    bit5~bit7:CDT
+    0x5c:
+    bit0~bit5:CDC
+    bit6~bit7:FFI
+    0x5c-low byte,0x5d-high byte
+*/
+/**@brief set globle parameters.FFI(First Filter Iterations ),CDC(Charge Discharge Current)
+ * @brief CDT(Charge Discharge Time),SFI(Second Filter Iterations),ESI(Electrode Sample Interval)
+ * @param 16bit value
+ * 
+ * */
+void Mpr121::set_globle_param(u16 value)
+{
+    u8 val_l=(u8)value;
+    u8 val_h=(u8)(value>>8);
+    IIC_write_byte(FILTER_AND_GLOBAL_CDC_CFG_ADDR,val_l);
+    IIC_write_byte(FILTER_AND_GLOBAL_CDT_CFG_ADDR,val_h);
+}
+
+
+/**@brief Set threshold  
+ * @brief Touch condition: Baseline - Electrode filtered data > Touch threshold
+ * @brief Release condition: Baseline - Electrode filtered data < Release threshold
+ * @brief It's not very necessary to set this param in simple use case.
+ * @param Threshold for sensor
+ * */
+void Mpr121::set_threshold(u16 threshold)
+{
+    u8 thres_touch=(u8)threshold;
+    u8 thres_realese=(u8)(threshold>>8);
+    for(int i=0;i<CHANNEL_NUM;i++)
+    {
+        IIC_write_byte(THRESHOLD_REG_START_ADDR+2*i,thres_touch);
+        IIC_write_byte(THRESHOLD_REG_START_ADDR+2*i+1,thres_realese);
+    }
+}
+
+
+
+/**@brief Total 12 channels for touch electrode,bit0-bit11 of 8+8 bits data indicate if there is a channel triggled.
+ * @brief Bit0-bit11 correponding to channel 0-11
+ * @return 16 bits indicate data.
+ * */
+u16 Mpr121::check_status_register()
+{
+    u16 val=0;
+    u8 val_l,val_h;
+    IIC_read_byte(TOUCH_STATUS_REG_ADDR_L,&val_l);
+    IIC_read_byte(TOUCH_STATUS_REG_ADDR_H,&val_h);
+    val=val_h<<8|val_l;
+    return val;
+}
+
+/**@brief When key is pressed,The sensor not just ouput 0/1
+ * @brief The MPR121 provides filtered electrode output data for all 13 channels,
+ * @param elecs_stat.Judge which channel is triggled according to this param 
+ * @param elecs_filtered_data.Get corresponding channel data.
+ * */
+void Mpr121::get_filtered_reg_data(u16 elecs_stat,u16* elecs_filtered_data)
+{
+    u16 value=0;
+    u8 data_l,data_h;
+    for(int i=0;i<CHANNEL_NUM;i++)
+    {
+        if(elecs_stat&(1<<i))
+        {
+            IIC_read_byte(FILTERED_DATA_REG_START_ADDR_L+2*i,&data_l);
+            IIC_read_byte(FILTERED_DATA_REG_START_ADDR_L+2*i+1,&data_h);
+            elecs_filtered_data[i]=(u16)data_h<<8|data_l;
+            Serial.print("press value= 0X");
+            Serial.println(elecs_filtered_data[i],HEX);
+        }
+    }
+}
+
+/**@brief Set stop mode by set ELEC_CFG_REG_ADDR to 0x0 
+ * 
+ * */
+void Mpr121::sensor_stop()
+{
+    IIC_write_byte(ELEC_CFG_REG_ADDR,0);
+}
+
+/**@brief Set start mode with proximity enable by set ELEC_CFG_REG_ADDR to 0x0 
+ * 
+ * */
+void Mpr121::sensor_start_proximity_enable()
+{
+    IIC_write_byte(ELEC_CFG_REG_ADDR,0x3c);
+}
+
+/**@brief Set start mode with proximity disable by set ELEC_CFG_REG_ADDR to 0x0 
+ * 
+ * */
+void Mpr121::sensor_start_proximity_disable()
+{
+    IIC_write_byte(ELEC_CFG_REG_ADDR,0x0c);
+}
+
+
+/*****************************************IIC operation interface!!*****************************************/
+/************************************************************************************************************/
+void Mpr121::IIC_write_byte(u8 reg,u8 byte)
+{
+    Wire.beginTransmission(_IIC_ADDR);
+    Wire.write(reg);
+    Wire.write(byte);
+    Wire.endTransmission();
+}
+
+void Mpr121::IIC_write_bytes(u8 reg,u8 bytes[],u32 bytes_len)
+{
+    Wire.beginTransmission(_IIC_ADDR);
+    Wire.write(reg);
+    for(int i=0;i<bytes_len;i++)
+    {
+        Wire.write(bytes[i]);
+    }
+    Wire.endTransmission();
+}
+
+void Mpr121::IIC_read_byte(u8 reg,u8* byte)
+{
+    Wire.beginTransmission(_IIC_ADDR);
+    Wire.write(reg);
+    Wire.endTransmission(false);
+
+    Wire.requestFrom(_IIC_ADDR,(u8)1);
+    while(1!=Wire.available());
+    *byte=Wire.read();
+    
+}
+
+void Mpr121::IIC_read_bytes(u8 start_reg,u8* bytes,u32 bytes_len)
+{
+    Wire.beginTransmission(_IIC_ADDR);
+    Wire.write(start_reg);
+    Wire.endTransmission(false);
+
+    Wire.requestFrom(_IIC_ADDR,(u8)bytes_len);
+     while(bytes_len!=Wire.available());
+    for(int i=0;i<bytes_len;i++)
+    {
+        bytes[i]=Wire.read();
+    }
+}
+
---
+++ Seeed_MPR121_driver.h
@@ -0,0 +1,129 @@
+/*
+ * Seeed_MPR121_driver.h
+ * Driver for DIGITAL I2C HUMIDITY AND TEMPERATURE SENSOR
+ *  
+ * Copyright (c) 2018 Seeed Technology Co., Ltd.
+ * Website    : www.seeed.cc
+ * Author     : downey
+ * Create Time: May 2018
+ * Change Log :
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef SEEED_MPR121_DRIVER_H
+#define SEEED_MPR121_DRIVER_H
+
+
+#include <Wire.h>
+#include <Arduino.h>
+
+#define DEFUALT_MPR121_ADDR   0X5B
+
+/****************************************************Sensor register address!!***********************************************/
+/****************************************************************************************************************************/
+
+
+#define CHANNEL_NUM   12
+
+/*These two registers indicate the detected touch/release status of all of the 12 sensing input channels*/
+#define TOUCH_STATUS_REG_ADDR_L             0X00
+#define TOUCH_STATUS_REG_ADDR_H             0X01
+
+/*The MPR121 provides filtered electrode output data for all 12 channels*/
+/*Total 26 registers,for 12 channel dlectrode out put data.Each channel corresponding two registers:high byte and low byte*/
+/*0x04~0x1d*/
+#define FILTERED_DATA_REG_START_ADDR_L      0X04
+#define FILTERED_DATA_REG_START_ADDR_H      0X05
+
+/*Along with the 10-bit electrode filtered data output, each channel also has a 10-bit baseline value*/
+/*0X1E~0X2A*/
+#define BASELINE_VALUE_REG_START_ADDR             0X1E
+
+
+/*All12 of the electrode baseline values are controlled by the same set of filtering control registers, 0x2B ~ 0x35*/
+/*The 13th channel ELEPROX is controlled by registers 0x36 ~ 0x40*/
+#define BASELINE_FILTERING_CONTROL_REG_START_ADDR    0X2B  
+
+
+/*Each of the 12 channels can be set with its own set of touch and release thresholds. Touch and release are detected by
+comparing the electrode filtered data to the baseline value. typically in the range 0x04~0x10*/
+/*Touch condition: Baseline - Electrode filtered data > Touch threshold
+  Release condition: Baseline - Electrode filtered data < Release threshold*/
+#define THRESHOLD_REG_START_ADDR            0X41
+
+/*All 12 channels use the same set of touch and release debounce numbers.*/
+#define DEBOUNCE_REG_ADDR                   0X5B
+
+/*These two registers set the global AFE settings. This includes global electrode charge/discharge current CDC, global charge/
+discharge time CDT, as well as a common filtering setting (FFI, SFI, ESI) for all 12 channels, including the 13th Eleprox channel*/
+#define FILTER_AND_GLOBAL_CDC_CFG_ADDR      0X5C
+#define FILTER_AND_GLOBAL_CDT_CFG_ADDR      0X5D
+
+/*0X5F-0X6B*/
+#define ELEC_CHARGE_CURRENT_REG_START_ADDR  0X5F
+
+/*0X6C-0X72*/
+#define ELEC_CHARGE_TIME_REG_START_ADDR  0X6C
+
+/*The Electrode Configuration Register (ECR) determines if the MPR121 is in Run Mode or Stop Mode*/
+/*Default is 0 to stop mode*/
+#define ELEC_CFG_REG_ADDR                    0X5E
+
+using u8 = unsigned char;
+using u16 = unsigned short;
+using u32 =long unsigned int;
+using s32 = int;
+
+typedef enum
+{
+    STOP_MODE,
+    START_PROXIMITY_ENABLE_MODE,
+    START_PROXIMITY_DISABLE_MODE,
+}sensor_mode_t;
+
+
+class Mpr121
+{
+    public:
+        Mpr121(u8 addr=DEFUALT_MPR121_ADDR);
+        ~Mpr121(){}
+        void begin();
+        u16 check_status_register();
+        void select_mode(sensor_mode_t mode);
+        void set_debounce(u8 debounce);
+        void set_threshold(u16 threshold);
+        void set_globle_param(u16 value);
+        void get_filtered_reg_data(u16 elecs_stat,u16* elecs_filtered_data);
+
+    private:        
+        u8 _IIC_ADDR;
+        void IIC_read_bytes(u8 reg,u8* bytes,u32 bytes_len);
+        void IIC_read_byte(u8 reg,u8* byte);
+        void IIC_write_byte(u8 reg,u8 byte);
+        void IIC_write_bytes(u8 reg,u8 bytes[],u32 bytes_len);
+        void sensor_stop();
+        void sensor_start_proximity_enable();
+        void sensor_start_proximity_disable();
+};
+
+
+
+#endif
