3f982d5cd6c4ceed13b544f8d322e695b5a9be95
linux-downey <1148256554@qq.com>
2018-05-14 14:40:15 +0800

    arduino example

--- examples/MPR121_demo/MPR121_demo.ino
+++ examples/MPR121_demo/MPR121_demo.ino
@@ -35,15 +35,27 @@ Mpr121 mpr121;
 u16 touch_status_flag[CHANNEL_NUM]={0};
 void setup()
 {
+  s32 ret=0;
   Serial.begin(115200);
-  mpr121.begin();
+  if(mpr121.begin()<0)
+  {
+    Serial.println("Can't detect device!!!!");
+  }
+  else
+  {
+    Serial.println("mpr121 init OK!");
+  }
   delay(100);
 }
 void loop()
 {
   u16 result=0;
+  u16 filtered_data_buf[CHANNEL_NUM]={0};
+  u8 baseline_buf[CHANNEL_NUM]={0};
   
   result=mpr121.check_status_register();
+
+  mpr121.get_filtered_reg_data(&result,filtered_data_buf);
 
   for(int i=0;i<CHANNEL_NUM;i++)
   {
