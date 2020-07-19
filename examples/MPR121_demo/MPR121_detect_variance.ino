1f5557717b9c79b46df6ccbe8eaef6872b929445
Ivan Fan <fanyfivan@gmail.com>
2020-07-19 22:01:53 +1000

    add demo regarding detecting variance value

---
+++ examples/MPR121_demo/MPR121_detect_variance.ino
@@ -0,0 +1,66 @@
+/*
+ * MPR121_demo.ino
+ * Driver for DIGITAL I2C HUMIDITY AND TEMPERATURE SENSOR
+ *
+ * Copyright (c) 2018 Seeed Technology Co., Ltd.
+ * Website    : www.seeed.cc
+ * Author     : downey
+ * Create Time: May 2018
+ * Change Log :
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include "Seeed_MPR121_driver.h"
+
+Mpr121 mpr121;
+
+u16 touch_status_flag[CHANNEL_NUM]={0};
+void setup()
+{
+    s32 ret=0;
+    Serial.begin(115200);
+    if(mpr121.begin()<0)
+    {
+        Serial.println("Can't detect device!!!!");
+    }
+    else
+    {
+        Serial.println("mpr121 init OK!");
+    }
+    delay(100);
+}
+void loop()
+{
+    u16 result=0;
+    u16 filtered_data_buf[CHANNEL_NUM]={0};
+    u8 baseline_buf[CHANNEL_NUM]={0};
+
+    result=mpr121.check_status_register();
+    mpr121.get_filtered_reg_data(&result,filtered_data_buf);
+
+    // selected channel
+    int selected_channel = 3;
+    Serial.print("key ");
+    Serial.print(selected_channel);
+    Serial.print(" value:");
+    Serial.println(filtered_data_buf[selected_channel]);
+    delay(50);
+}
